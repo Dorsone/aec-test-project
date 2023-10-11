55afff2e467a9e3d181a6d5a1dca9a463dad7885
Jasur Dustmurodov <d.jasur@mga.team>
2023-10-11 21:58:08 +0500

    unit test restored

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Artisan;
+use JetBrains\PhpStorm\NoReturn;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
